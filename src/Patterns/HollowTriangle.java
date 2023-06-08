da95b7b8beeb7942a7601a8154e0ef8443ae1c18
K Pavan Kumar Reddy <001iampavankumar@gmail.com>
2023-06-08 22:19:00 +0530

    Added Comments

--- src/Patterns/HollowTriangle.java
+++ src/Patterns/HollowTriangle.java
@@ -9,15 +9,24 @@ public class HollowTriangle {
 
         int n = input.nextInt();
 
+
+        //defines no of rows
         for(int row = 1;row<=n;row++){
+
+
+            //printing spaces before every row
             for(int spaces = 0;spaces<n-row;spaces++){
                 System.out.print(" ");
             }
+
+            //taking care of coloums
             for(int col = 1;col<=row;col++){
 
                 if(row == 1||row==n||col == row||col==1){
                     System.out.print("* ");
                 }
+
+                //taking care of inside spaces of a triangle
                 else{
                     System.out.print("  ");
                 }
