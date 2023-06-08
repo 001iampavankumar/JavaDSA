ab7330e2d131c79b0ba356ed938174a9378de2e5
K Pavan Kumar Reddy <001iampavankumar@gmail.com>
2023-06-08 22:16:26 +0530

    Initial commit

---
+++ src/Patterns/HollowTriangle.java
@@ -0,0 +1,31 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class HollowTriangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+
+        int n = input.nextInt();
+
+        for(int row = 1;row<=n;row++){
+            for(int spaces = 0;spaces<n-row;spaces++){
+                System.out.print(" ");
+            }
+            for(int col = 1;col<=row;col++){
+
+                if(row == 1||row==n||col == row||col==1){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+
+            }
+            System.out.println();
+        }
+
+
+    }
+}
