d12a8117a52fa0c04053a5707ae210e4055bd677
K Pavan Kumar Reddy <001iampavankumar@gmail.com>
2023-06-22 13:34:55 +0530

    Initial Commit

---
+++ src/Arrays/ChallengingOnes/MaxAndMinSumOfNMinusONeElements.java
@@ -0,0 +1,42 @@
+package Arrays.ChallengingOnes;
+
+import java.util.Scanner;
+
+public class MaxAndMinSumOfNMinusONeElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Size: ");
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        System.out.print("enter " + n + " Elements: ");
+
+        for (int i = 0; i < a.length; i++) {
+
+            a[i] = sc.nextInt();
+        }
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for(int i = 0;i<a.length;i++){
+            for(int j = 0;j<a.length;j++){
+                if(i!=j){
+                    sum = sum + a[j];
+                }
+            }
+            if(sum>max){
+                max = sum;
+            }
+            if(min>sum){
+                min = sum;
+            }
+            sum = 0;
+        }
+
+        System.out.println(min +" "+ max);
+
+    }
+
+}
