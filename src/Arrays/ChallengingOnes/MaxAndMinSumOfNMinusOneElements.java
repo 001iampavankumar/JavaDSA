681e7c0e82da6b87bfcd5af6d060904f518e895f
K Pavan Kumar Reddy <001iampavankumar@gmail.com>
2023-06-22 13:35:50 +0530

    Initial Commit

--- src/Arrays/ChallengingOnes/MaxAndMinSumOfNMinusOneElements.java
+++ src/Arrays/ChallengingOnes/MaxAndMinSumOfNMinusOneElements.java
@@ -2,7 +2,7 @@ package Arrays.ChallengingOnes;
 
 import java.util.Scanner;
 
-public class MaxAndMinSumOfNMinusONeElements {
+public class MaxAndMinSumOfNMinusOneElements {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Size: ");
