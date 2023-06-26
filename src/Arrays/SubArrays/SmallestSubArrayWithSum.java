895e4b28dce2b682a8da42970296cf5876b1a861
K Pavan Kumar Reddy <001iampavankumar@gmail.com>
2023-06-26 15:56:30 +0530

    need changes

--- src/Arrays/SubArrays/SmallestSubArrayWithSum.java
+++ src/Arrays/SubArrays/SmallestSubArrayWithSum.java
@@ -2,7 +2,61 @@ package Arrays.SubArrays;
 import java.util.Scanner;
 
 public class SmallestSubArrayWithSum {
+
+    static void smallestSubArray(int[] a,int res){
+
+        //count for finding minimum sub array
+        int count = a.length+1;
+
+        //find start index of sub array
+        int start = -1;
+
+        //find end index of sub array
+        int end = -1;
+        //System.out.println("hi");
+
+        for(int i = 0;i<a.length;i++){
+            for(int j = i;j<a.length;j++){
+                int sum = 0;
+
+                //calculating sum
+                for(int k = i;k<=j;k++){
+                    sum = sum + a[k];
+                }
+
+                //checking sum with given sum
+                if(sum == -res || sum==res){
+                    //count is for finding minimum sub array
+                    if(count>j-i+1){
+                        start = i;
+                        end = j;
+                        count = j-i+1;
+                    }
+                }
+            }
+        }
+
+        if(count==a.length+1){
+            return;
+        }
+        for(int i = start;i<=end;i++){
+            System.out.print(a[i]+" ");
+        }
+        return;
+    }
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for(int i = 0;i<a.length;i++){
+            a[i] = sc.nextInt();
+        }
+        smallestSubArray(a,k);
+
+
+
 
     }
 }
