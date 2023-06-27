ae6c236b4132acf8c308f6eb4251f26ce31af8c8
K Pavan Kumar Reddy <001iampavankumar@gmail.com>
2023-06-27 10:00:58 +0530

    First Commit

--- src/Arrays/SubArrays/SmallestSubArrayWithSum.java
+++ src/Arrays/SubArrays/SmallestSubArrayWithSum.java
@@ -37,7 +37,8 @@ public class SmallestSubArrayWithSum {
         }
 
         if(count==a.length+1){
-            System.out.println("");
+            System.out.println("No subarray found.");
+            return;
         }
         for(int i = start;i<=end;i++){
             System.out.print(a[i]+" ");
@@ -46,8 +47,11 @@ public class SmallestSubArrayWithSum {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter sum: ");
         int k = sc.nextInt();
+        System.out.print("Enter array size: ");
         int n = sc.nextInt();
+        System.out.println("Enter the elements: ");
         int[] a = new int[n];
         for(int i = 0;i<a.length;i++){
             a[i] = sc.nextInt();
