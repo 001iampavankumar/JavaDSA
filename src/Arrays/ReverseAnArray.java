790d2d4669f8f3a48add5c6b97c04054d6e8ea69
K Pavan Kumar Reddy <001iampavankumar@gmail.com>
2023-07-01 20:35:16 +0530

    First Commit

--- src/Arrays/ReverseAnArray.java
+++ src/Arrays/ReverseAnArray.java
@@ -3,8 +3,9 @@ package Arrays;
 import java.util.Scanner;
 
 public class ReverseAnArray {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int[] a = new int[n];
 
